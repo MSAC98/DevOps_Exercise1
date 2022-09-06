0358114f834ed63670815f428282fbf447d34ab9
MSAC98 <44638120+MSAC98@users.noreply.github.com>
2022-09-06 10:28:22 +0200

    d3

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -7,6 +7,8 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
   let button = screen.getByRole("button");
   expect(button).toHaveTextContent("Login");
+  let usernameField = screen.getByLabelText("username");
+  expect(usernameField).toHaveAttribute("placeholder","indtast brugernavn")
 });
 
 test('has login button', ()=>{
